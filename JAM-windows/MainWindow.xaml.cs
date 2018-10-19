1d3f21a0b6502ebb1db93644bd4e1facb9e9195c
Jacob Zimmer <32368022+jlzimmer@users.noreply.github.com>
2018-10-18 22:41:43 -0500

    Feature/ui v1 (#1)
    * Begin work on WPF user interface
    
    Created overall grid layout and tab control
    
    NOTE: all elements should be Segoe UI 14 unless otherwise noted. Place button text inside label elements
    
    * Added menu, OneDrive tab
    
    Now has a docked menu with items, OneDrive tab has connection button and a storage bar. Note window is now resizable with minimum dimensions 500x400
    
    * Added ListView to OneDrive tab
    
    Also fixed spacing on margins, and restructured DockView
    
    * Mirrored UI on Google and Box tabs
    
    Also fixed cloud usage bar to stretch with window resizing. Note: ListView still has issues with columns filling entire view

---
+++ JAM-windows/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace JAM_windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Initialize directory if known
+        }
+    }
+}
