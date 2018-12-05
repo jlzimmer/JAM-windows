1566b975aef7ed9f4f6f4ea53b507cb3caaa3618
Jacob Zimmer <32368022+jlzimmer@users.noreply.github.com>
2018-12-04 18:29:11 -0600

    Feature/dir select (#2)
    * Created SelectDir.cs and added FolderBrowserDialog
    
    Windows.Forms was added to references for use, and minor renaming occurred to conform to C# standards. Proper access control is in place on SelectDir properties and constructor is returning the proper directory path
    
    * Update JAM-windows/MainWindow.xaml.cs
    
    Added line 29 (changes the UI label to show selected directory)
    
    * Updated SelectDir.cs
    
    Added the GetDirSize() method and converted DirSize to a computed property
    
    * Added formatted size field to MainWindow
    
    By calling the SelectDir.SizeFormat() method, the window now shows the size of the selected directory with appropriate rounding and suffix. Exception handling in GetDirSize() needs work
    
    * Added guard statements
    
    Refactored directory selection interaction logic to be safer; exception handling still needs to be addressed
    
    * Update JAM-windows/SelectDir.cs
    
    Changed object types in try/catch block for better parsing safety
    
    * Added documentation on the SelectDir class
    
    * Directory crawling using Stack<T>
    
    Directory traversal now works. Added commentary explaining this process, future commit should address logging directories or files that threw access exceptions.

---
+++ JAM-windows/SelectDir.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace JAM_windows
+{
+    class SelectDir
+    {
+        // Properties
+        // ***
+        // PathToDir - the directory root selected by the user via dialog window that appears during class construction
+        // DirSize - calculated value that calls GetDirSize() each time the property is accessed, this is to ensure that any external changes are accounted for
+        // Paths - a list of FileInfo objects that represent all *accessible* paths, this list should only be set by GetDirSize()
+        // ***
+        public readonly string PathToDir;
+        public long DirSize { get => GetDirSize(PathToDir); }
+        public List<FileInfo> Files { get; }
+        
+        // ***
+        // Returns SelectDir object on success, null on failure
+        // Uses a System.Windows.Forms.FolderBrowserDialog() object to fetch the initial root directory and assigns it to PathToDir
+        public SelectDir()
+        {
+            using (FolderBrowserDialog browserDialog = new FolderBrowserDialog())
+            {
+                browserDialog.Description = "Select a local directory for cloud file synchronization.";
+                browserDialog.RootFolder = Environment.SpecialFolder.Desktop;
+
+                if (browserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    PathToDir = browserDialog.SelectedPath;
+                }
+                else
+                {
+                    PathToDir = null;
+                }
+            }
+        }
+
+        // ***
+        // Returns a long int representing the total size of the root directory
+        // This method is called each time the DirSize field is accessed to give an accurate and up-to-date read of the size of the working directory
+        // Also builds a List<FileInfo> so that subsequent logical interactions on the list of valid file paths can be executed immediately without calling this method again
+        private static long GetDirSize(string root)
+        {
+            long size = 0;
+            Stack<string> paths = new Stack<string>();
+            List<FileInfo> files = new List<FileInfo>();
+
+            if (!Directory.Exists(root))
+            {
+                throw new ArgumentException(String.Format("{0} is an invalid root directory path", root));
+            }
+            paths.Push(root);
+
+            while (paths.Count > 0)
+            {
+                string current = paths.Pop();
+
+                // Add all subdirectories of the current root to an array
+                // Verify they are accessible and push them onto the Stack<string>
+                string[] dPaths = null;
+                try
+                {
+                    dPaths = Directory.GetDirectories(current);
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    // DO STUFF
+                    continue;
+                }
+                catch (DirectoryNotFoundException e)
+                {
+                    // DO MORE STUFF
+                    continue;
+                }
+                foreach (string d in dPaths)
+                {
+                    paths.Push(d);
+                }
+
+                // Add paths of all files in the current directory to an array
+                // Create FileInfo objects and add it to the List after verifying they can be accessed
+                string[] fPaths = null;
+                try
+                {
+                    fPaths = Directory.GetFiles(current);
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    // SOMEONE FIGURE OUT HOW TO HANDLE THIS
+                    continue;
+                }
+                catch (DirectoryNotFoundException e)
+                {
+                    // JUST LOG IT OR WHATEVER
+                    continue;
+                }
+                foreach (string f in fPaths)
+                {
+                    try
+                    {
+                        FileInfo fileInfo = new FileInfo(f);
+                        files.Add(fileInfo);
+                        size += fileInfo.Length;
+                    }
+                    catch (FileNotFoundException e)
+                    {
+                        // DOCUMENT ERRORS SOMEWHERE
+                        continue;
+                    }
+                }
+            }
+
+            return size;
+        }
+
+        // ***
+        // Returns a string that is displayed in the bottom right corner of the MainWindow view
+        // Truncates the size of the root directory to one decimal point and add the appropriate suffix
+        public string SizeFormat()
+        {
+            string[] suffix = new string[] { "B", "KB", "MB", "GB" };
+            double size = DirSize;
+            int index = 0;
+
+            while (size / 1000 >= 1 || index < 3)
+            {
+                size /= 1000;
+                index++;
+            }
+
+            size = Math.Round(size, 1);
+            string info = size + " " + suffix[index];
+
+            return info;
+        }
+    }
+}
