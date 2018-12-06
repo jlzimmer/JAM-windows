74b0bf9b3e8c8604ccfdbfd8cc9fff8000dd0663
Zach Watson <35540447+ZachWatsonMiz@users.noreply.github.com>
2018-12-05 18:56:32 -0600

    added new librarys and need to redirect back to app when finished allowing app to make changes

--- JAM-windows/ConnectBox.cs
+++ JAM-windows/ConnectBox.cs
@@ -17,6 +17,15 @@ using System.Web;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using Box.V2.Converter;
+using Box.V2.Exceptions;
+using Box.V2.Extensions;
+using Box.V2.JWTAuth;
+using Box.V2.Managers;
+using Box.V2.Models;
+using Box.V2.Plugins;
+using Box.V2.Request;
+using Box.V2.Services;
 
 namespace JAM_windows
 {
@@ -29,23 +38,33 @@ namespace JAM_windows
         public OAuthSession BoxAuthenicatedSession { get; set; }
        // OAuthSession session = new OAuthSession();
         
-          static BoxConfig config = new BoxConfig("1iymr4r3dlqpw44519tj7i5ycglmnzeq", "ujKUZqvE4SzHoNzs2JOUupxJemQWjW5m", new Uri("ttp://localhost:5000/route/return"));
+          static BoxConfig config = new BoxConfig("1iymr4r3dlqpw44519tj7i5ycglmnzeq", "ujKUZqvE4SzHoNzs2JOUupxJemQWjW5m", new Uri("http://localhost:5000/route/return"));
         //  BoxClient client = new BoxClient(config);
 
         static string clientIdParam = $"client_id={config.ClientId}";
         static string redirectUrlParam = $"redirect_uri={BOX_AUTH_URL}";
 
         HttpClient web = new HttpClient();
-        Uri webURL = new Uri ($"{BOX_AUTH_URL}?response_type=code&" + clientIdParam);
-       
+        Uri webURL = new Uri ($"{BOX_AUTH_URL}?response_type=code&" + clientIdParam +"&" +config.RedirectUri);
+        BoxClient client = new BoxClient(config);
+
         public void InitClient()
-        {
-            BoxClient client = new BoxClient(config);
+        {    
             // BoxClient client = new BoxClient(configuration);
             Console.WriteLine("Client Initiated");
-            web.BaseAddress = webURL;
+           // web.BaseAddress = webURL;
             System.Diagnostics.Process.Start(webURL.ToString());
-            
+            //   OAuthSession boxSession;
+            GetInfo();
+
+        }
+        public async void GetInfo()
+        {
+            //get auth code
+
+            //client.Auth.AuthenticateAsync("needs auth code here");
+            BoxFile file = new BoxFile();
+          //  file = await client.FilesManager.GetInformationAsync(id: "1");
         }
     }
 
